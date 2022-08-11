b68a1c96ed755cfcf84a39a68feb0c2f35dfdcf9
William Luck <luck.william.h@gmail.com>
2022-08-11 14:00:26 -0500

    commit tree a7cb96c8273d5aba6c1dcb36c69ce51491dca588

--- lib/song.rb
+++ lib/song.rb
@@ -49,4 +49,38 @@ class Song
     song.save
   end
 
+  def self.new_from_db(row)
+    # From my own understanding, since this is a class method, self.new is the same as Song.new, and we are initializing it with the values obtained from a particular row in the db table. 
+    self.new(id: row[0], name: row[1], album: row[2])
+  end
+
+  def self.all
+    # grabs all the songs from the database. This is a better alternative to declaring @@all, because we are persisting the data to the database. Will still be there if the code stops running. 
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+    SQL
+
+    # accessing the database connection from environment.rb file
+    # Returns an array of rows from teh database that matches the query, which we can then map over to create a new ruby object for each row
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  # The only difference between the above is that we are give the method an argument, and including that name in the SQL query to retrieve it. The .first method just returns the first matching name.
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+
 end
